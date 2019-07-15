3c37a1c10a65c5dd1af2026165a6b9474976828a
Daniel B <busisd@carleton.edu>
2019-07-15 19:28:52 +0100

    Now, scrolls the chatbox for the user if they are at the bottom when a new message comes in. Also, the chatbox formatting is correct.

--- static/chatroom.js
+++ static/chatroom.js
@@ -8,23 +8,43 @@ comment_box.addEventListener("keyup", function(event) {
 });
 
 function sendMessage() {
-	socket.emit('post_comment', {username:username_box.value, message:comment_box.value});
-	comment_box.value="";
+	if (comment_box.value!==""){
+		socket.emit('post_comment', {username:username_box.value, message:comment_box.value});
+		comment_box.value="";
+	}
+}
+
+
+/*
+Keeps scroll box scrolled to the bottom
+*/
+var chat_log_div = document.getElementById("chat_log_div");
+function updateScroll(){
+    chat_log_div.scrollTop = chat_log_div.scrollHeight;
 }
 
 var chat_log = document.getElementById("chat_log");
 socket.on('new_comment', function(comment_data){
+	var shouldScroll = false;
+	if (chat_log_div.offsetHeight+chat_log_div.scrollTop+1 >= chat_log_div.scrollHeight){
+		shouldScroll = true;
+	}
+	console.log(chat_log_div.offsetHeight+chat_log_div.scrollTop)
+	console.log(chat_log_div.scrollHeight)
+	
 	new_chat_row = document.createElement("tr");
 	new_chat = document.createElement("td");
 	username = document.createElement("span");
+	username.className = "Username"
 	separator = document.createElement("span");
+	separator.className = "Separator"
 	message = document.createElement("span");
+	message.className = "Message"
 	if (comment_data["username"]!==""){
 		username.innerText=comment_data["username"];
 	} else {
 		username.innerText="Anonymous";
 	}
-	username.className = "Username" 
 	separator.innerText=": ";
 	message.innerText=comment_data["message"];
 	new_chat.appendChild(username);
@@ -32,6 +52,10 @@ socket.on('new_comment', function(comment_data){
 	new_chat.appendChild(message);
 	new_chat_row.appendChild(new_chat);
 	chat_log.appendChild(new_chat_row);
+	
+	if (shouldScroll) {
+		updateScroll();
+	}
 });
 
 
